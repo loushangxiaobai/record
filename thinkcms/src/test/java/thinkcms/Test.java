5262911972a997dad867da375889320f6c1e0d53
xiaobai <274398014@qq.com>
2017-03-30 20:16:22 +0800

    短信发送测试

--- thinkcms/src/test/java/thinkcms/Test.java
+++ thinkcms/src/test/java/thinkcms/Test.java
@@ -1,13 +1,41 @@
 package thinkcms;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws HttpException,IOException{
+	
+		HttpClient clinet = new HttpClient();
+		PostMethod post= new PostMethod("http://utf8.sms.webchinese.cn");
+		post.addRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+		NameValuePair[] data={
+				new NameValuePair("key","19f7a515311ef3ec1b42"),
+				new NameValuePair("smsMob", "17688867993"),
+				new NameValuePair("smsText", "备案系统短信测试")
+				
+		};
+		post.setRequestBody(data);
+		clinet.executeMethod(post);
+		Header[] headers=post.getResponseHeaders();
+		int statusCode =post.getStatusCode();
+		System.out.println("statusCosde:" +statusCode);
 		
-		ArrayList<String> list = new ArrayList<>();
-		list.add("111111");
-		System.out.println(list);
+		for(Header h : headers){
+			System.out.println("----- "+h.toString());
+			
+		}
+		
+		String result = new String(post.getResponseBodyAsString().getBytes("utf-8"));
+	
+		System.out.println(result);
 	}
 
 }
